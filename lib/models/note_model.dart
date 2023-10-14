9c3203c5b5abfe5027700fabf8e2e26a04ea6fc5
BishoyEssam <bishoyessam1212@gmail.com>
2023-10-14 14:08:04 +0300

    Creating note_model.g

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,17 @@
 // ignore_for_file: file_names
 
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel({
